4743359e0b92609b953f64166810e2d70962b04b
turi <turinguc@gmail.com>
2018-06-05 14:03:13 +0800

    add dun1.go

--- GoLanguageProgramming/OneSection/induction/dup1.go
+++ GoLanguageProgramming/OneSection/induction/dup1.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	`
+	扫描器 bufio 里面的NewScanner扫描器从程序的标准输入,它可以以行或者单词为单位断开,这是以行为单位处理的最简单的方式.
+	`
 	input := bufio.NewScanner(os.Stdin)
 	fmt.Print(">>> ")
 	for input.Scan() {
